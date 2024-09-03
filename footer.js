68c79bad96af92c949ccde2bfade0f62ce4d44b1
Ahsan-codeCraft <ahsanwork360@gmail.com>
2024-09-03 18:07:05 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-var pos = document.documentElement;
+var pos = document.querySelector(".ahsan_container");
 pos.addEventListener('mousemove', e=>{
     pos.style.setProperty('--x', e.clientX+ 'px')
     pos.style.setProperty('--y', e.clientY+ 'px')
