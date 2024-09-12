db3b52f6ea494ecd58dfa3c8b7c21c3b3e189344
Awais297 <ahsanwork360@gmail.com>
2024-09-12 09:09:39 +0500

    Footer added

--- .history/contact_20240912085801.js
+++ .history/contact_20240912085801.js
@@ -313,27 +313,3 @@ if (window.innerWidth > 500) {
     });
 
 }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var container = document.querySelector(".text-wrapper");
-    var lightEffect = container.querySelector(".footer_cursor-light"); // Ensure it's selected within .text-wrapper
-
-    function updateTorchPosition(x, y) {
-        if (lightEffect) {
-            lightEffect.style.setProperty('--x', `${x}px`);
-            lightEffect.style.setProperty('--y', `${y}px`);
-        }
-    }
-
-    function handleMouseMove(e) {
-        var rect = container.getBoundingClientRect();
-        var x = e.clientX - rect.left;
-        var y = e.clientY - rect.top;
-        updateTorchPosition(x, y);
-    }
-
-    container.addEventListener('mousemove', handleMouseMove);
-    container.addEventListener('touchmove', handleMouseMove);
-    container.addEventListener('touchstart', handleMouseMove);
-});
